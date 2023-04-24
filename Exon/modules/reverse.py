320579fe542f0624b6aacc31bbd002f7fe76b0cf
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2023-04-24 23:08:37 +0530

    Update reverse.py

--- Exon/modules/reverse.py
+++ Exon/modules/reverse.py
@@ -51,7 +51,7 @@ def reverse(update, context):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton(text="Link", url=web_url)
+                            InlineKeyboardButton(text="⚡Link", url=web_url)
                         ]
                     ]
                 )
